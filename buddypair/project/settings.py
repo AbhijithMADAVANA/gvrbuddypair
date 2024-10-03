846107b1df9ec72d4312409033425ecb228c0bee
AMAL <amalmk2k16@gmail.com>
2024-10-03 16:31:36 +0530

    ...

--- buddypair/project/settings.py
+++ buddypair/project/settings.py
@@ -28,6 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()  # Reads the .env file
+
 
 # Application definition
 
@@ -104,12 +109,74 @@ AUTH_USER_MODEL = "U_auth.costume_user"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite31',
+#     }
+# }
+
+# Database Configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite31',
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
+
+# AWS S3 Configuration
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_SIGNATURE_NAME = env('AWS_S3_SIGNATURE_NAME')
+AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
+AWS_S3_FILE_OVERWRITE = env.bool('AWS_S3_FILE_OVERWRITE')
+AWS_DEFAULT_ACL = env('AWS_DEFAULT_ACL')
+AWS_S3_VERITY = env.bool('AWS_S3_VERITY')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Razorpay Configuration
+RAZORPAY_KEY_ID = env('RAZORPAY_KEY_ID')
+RAZORPAY_KEY_SECRET = env('RAZORPAY_KEY_SECRET')
+
+# Email Configuration
+EMAIL_BACKEND = env('EMAIL_BACKEND')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env.int('EMAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS')
+EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL')
+
+# import pymysql
+# pymysql.install_as_MySQLdb()
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', 
+#         # 'ENGINE': 'mysql.connector.django', 
+#         'NAME': 'buddypairdata',
+#         'USER': 'buddypair',
+#         'PASSWORD': 'Buddypair1234',
+#         'HOST': 'buddypairdata.c5cua2m4ci88.ap-south-1.rds.amazonaws.com',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#     }
+# }
+
+# AWS_ACCESS_KEY_ID ='AKIA4WJPWNLSPPZ4KBV7'
+# AWS_SECRET_ACCESS_KEY='72HcJZGMzuCrOYMMrxP17Mh9rqZOQhur6nLnjHF'
+# AWS_STORAGE_BUCKET_NAME='buddypairs3bucket'
+# AWS_S3_SIGNATURE_NAME='s3v4'
+# AWS_S3_REGION_NAME='ap-south-1'
+# AWS_S3_FILE_OVERWRITE=False
+# AWS_DEFAULT_ACL=None
+# AWS_S3_VERITY=True
+# DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 
 
 # Password validation
@@ -202,17 +269,17 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-RAZORPAY_KEY_ID = 'rzp_test_cc7cPplZgxsSFN'
-RAZORPAY_KEY_SECRET = 'HRh6AjXf6R4tah0dGmt5Kole'
+# RAZORPAY_KEY_ID = 'rzp_test_cc7cPplZgxsSFN'
+# RAZORPAY_KEY_SECRET = 'HRh6AjXf6R4tah0dGmt5Kole'
 
 # LOGIN_REDIRECT_URL = 'auth_page'
 # LOGOUT_REDIRECT_URL = 'auth_page'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shabeebmohammedpes@gmail.com'
-EMAIL_HOST_PASSWORD = 'etjv byky zrkb ctct'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'shabeebmohammedpes@gmail.com'
+# EMAIL_HOST_PASSWORD = 'etjv byky zrkb ctct'
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False 
